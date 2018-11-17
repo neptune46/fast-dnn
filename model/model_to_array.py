080c51e0c2147837f993838e991f03ecd82f51d6
neptune46 <neptune46@outlook.com>
2018-11-17 15:32:03 +0800

    parse pb model file and convert weights/bias into c++ array

---
+++ model/model_to_array.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+from tensorflow.python.platform import gfile
+from tensorflow.python.framework import tensor_util
+
+def saveList(f, l):
+  f.write("{\n")
+  for x in l:
+    f.write(str(x)+", \n")
+  f.write("},\n")
+
+def save1D(f, l, name):
+  dim = "["+ str(len(l)) + "]"
+  f.write("float " + name + dim + " = \n")
+  saveList(f, l)
+
+def save2D(f, ll, name):
+  dim = "["+ str(len(ll)) + "]" + "["+ str(len(ll[0])) + "]"
+  f.write("float " + name + dim + " = {\n")
+  for l in ll:
+    saveList(f, l)
+  f.write("}\n")
+
+def dumpArray(name, array):
+  a = name.split('/')
+  arrayname = a[0] + "_" + a[1]
+  filename = arrayname + ".cpp"
+  l = array.tolist()
+  with open(filename, 'w+') as f:
+    if len(array.shape) == 1:
+      save1D(f, l, arrayname)
+    elif len(array.shape) == 2:
+      save2D(f, l, arrayname)
+    f.close()
+  print(arrayname)
+
+GRAPH_PB_PATH = "data\\mnist.pb" #path to your .pb file
+with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
+  print("load graph")
+  with gfile.FastGFile(GRAPH_PB_PATH,'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    sess.graph.as_default()
+    tf.import_graph_def(graph_def, name='')
+    graph_nodes=[n for n in graph_def.node]
+
+    wts = [n for n in graph_nodes if n.op=='Const']
+
+    for n in wts:
+      print ("Name of the node - %s" % n.name)
+      tensor1 = n.attr['value'].tensor
+      array1 = tensor_util.MakeNdarray(tensor1)
+      print (array1.shape, array1.size)
+      if (array1.size > 1):
+        dumpArray(n.name, array1)
+
+print('done')
